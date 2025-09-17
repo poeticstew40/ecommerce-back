54d957dc4175f68a8f869528d6430fb1eb790b0e
poeticstew40 <nicolas_gigena@hotmail.es>
2025-09-17 20:44:09 -0300

    creacion dtos

---
+++ src/main/java/back/ecommerce/dtos/ProductosRequest.java
@@ -0,0 +1,19 @@
+package back.ecommerce.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ProductosRequest {
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+    private String categoriaId;
+}
---
+++ src/main/java/back/ecommerce/dtos/UsuariosResponse.java
@@ -0,0 +1,14 @@
+package back.ecommerce.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class UsuariosResponse {
+
+}
