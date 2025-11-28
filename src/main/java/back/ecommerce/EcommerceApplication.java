62cff84346d198b99046146737ad9742502d035d
poeticstew40 <nicolas_gigena@hotmail.es>
2025-11-28 02:14:44 -0300

    prueba despliegue automatico

--- src/main/java/back/ecommerce/EcommerceApplication.java
+++ src/main/java/back/ecommerce/EcommerceApplication.java
@@ -13,7 +13,7 @@ public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
-		System.out.println("E-commerce application started successfully.");
+		System.out.println("E-commerce application started successfully. ");
 	}
 
 
